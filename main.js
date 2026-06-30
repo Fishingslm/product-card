0ac423d01c04a21fd5b467679eadacf228a54eb5
Fishingslm <khanmarat@gmail.com>
2026-06-30 22:44:58 +0500

    подключил JS в проект. реализовал функции по клику

---
+++ main.js
@@ -0,0 +1,58 @@
+// изменение цвета первой карточки по клику на кнопку
+
+const productCard = document.querySelector('.products__item.card');
+const changeCardColorButton = document.querySelector('#change-card-color-button');
+
+changeCardColorButton.addEventListener('click', () => {
+  productCard.style.backgroundColor = 'red';
+});
+
+// изменение цвета всех карточек по клику на кнопку
+
+const allProductCards = document.querySelectorAll('.products__item.card');
+const changeAllCardColorButton = document.querySelector('#change-all-cards-color-button');
+
+changeAllCardColorButton.addEventListener('click', () => {
+  allProductCards.forEach((card) => card.style.backgroundColor = 'blue');
+});
+
+// открыть google в новом окне по клику на кнопку
+
+const openGoogleButton = document.querySelector('#open-google-button');
+
+openGoogleButton.addEventListener('click', openGoogle);
+
+function openGoogle() {
+  const answer = confirm('Вы хотите открыть страницу Google.com?');
+  if (answer === true) {
+    window.open('https://www.google.com');
+  } else {
+    return;
+  }
+}
+
+//вывод в консоль лог
+
+const outputLogButton = document.querySelector('#output-console-log');
+
+outputLogButton.addEventListener('click', () => outputConsoleLog('ДЗ №6'))
+
+function outputConsoleLog(message) {
+  alert(message);
+  console.log(message);
+}
+
+//Вывод текста заголовка в консоль при наведении курсора мыши
+
+const title = document.querySelector('.title.page__title');
+
+title.addEventListener('mouseover', () => {
+  console.log(title.textContent);
+});
+
+//Изменение цвета кнопки при нажатии на нее
+const switchColorButton = document.querySelector('#switch-color-button');
+
+switchColorButton.addEventListener('click', () => {
+  switchColorButton.classList.toggle('switch-color-button--active');
+});
